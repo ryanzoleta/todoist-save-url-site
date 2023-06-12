09170441db625ea646904008f624251322e29b28
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-06-12 11:01:22 +0800

    Setup basic pages

---
+++ src/app/integration/complete/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {};
+
+const IntegrationCompletePage = (props: Props) => {
+  return (
+    <div>
+      <p>IntegrationCompletePage</p>
+      <p>This is where the user will be redirected to once they authorize the extension</p>
+    </div>
+  );
+};
+
+export default IntegrationCompletePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hi next</h1>
+      <h1 className="text-3xl text-center font-bold">Save URL to Todoist</h1>
+      <p className="text-center">A simple browser extension for easily saving URLs to Todoist</p>
     </main>
   );
 }
